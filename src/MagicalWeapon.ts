9e1b6a80019d3cdc3cb3b4e6ef44d4e15519abc8
Steven Diamante <stevendiamante13@gmail.com>
2026-06-25 00:08:56 -0400

    r - name weapon durability loss constant

--- src/MagicalWeapon.ts
+++ src/MagicalWeapon.ts
@@ -1,6 +1,8 @@
 import { MagicalObject } from './MagicalObject.ts';
 import type { Character } from './Character.ts';
 
+const DURABILITY_LOSS_PER_USE = 1;
+
 export class MagicalWeapon extends MagicalObject {
   constructor(
     maxHealth: number,
@@ -11,6 +13,6 @@ export class MagicalWeapon extends MagicalObject {
 
   override attack(target: Character): void {
     target.takeDamage(this.damage);
-    this.takeDamage(1);
+    this.takeDamage(DURABILITY_LOSS_PER_USE);
   }
 }
